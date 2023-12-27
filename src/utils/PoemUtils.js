292d13a765ad02c939127cce01745310d1602d2d
carellice <100231222+carellice@users.noreply.github.com>
2023-12-27 17:11:51 +0100

    update

--- src/utils/PoemUtils.js
+++ src/utils/PoemUtils.js
@@ -127,6 +127,131 @@ export const poems = [
         poem: "Chi è sensibile, attribuisce un'importanza enorme ai dettagli più insignificanti del comportamento altri, quelli che generalmente sfuggono alle persone normali.",
         author: AuthorUtils.SIGMUND_FREUD
     },
+    {
+        title: "",
+        poem: "Aveva il sorriso contagioso di chi aveva sofferto tanto. Era dannatamente bella, vestita dei suoi sbagli.",
+        author: AuthorUtils.CHARLES_BUKOWSKI
+    },
+    {
+        title: "",
+        poem: "Invano il sognatore rovista nei suoi vecchi sogni, coma fra la cenere, cercandovi una piccola scintilla per soffiarci sopra e riscaldare col fuoco rinnovato il proprio cuore freddo, e far risorgere ciò che prima commuoveva la sua anima.",
+        author: AuthorUtils.DOSTOEVSKIJ
+    },
+    {
+        title: "",
+        poem: "E coloro che furono visti danzare vennero giudicati pazzi da quelli che non potevano sentire la musica.",
+        author: AuthorUtils.NIETZSCHE
+    },
+    {
+        title: "",
+        poem: "I sogni si fanno di notte e si completano di giorno.",
+        author: AuthorUtils.SVEVO
+    },
+    {
+        title: "",
+        poem: "non c'è intimità più grande di poter essere sé stessi senza dover dimostrare di essere sempre all'altezza.",
+        author: AuthorUtils.ANONIMO
+    },
+    {
+        title: "Storie di ordinaria follia",
+        poem: "L'anima libera è rara, ma la riconosci quando la vedi - essenzialmente perchè ti senti bene, molto bene, quando sei vicino a lei o insieme a lei.",
+        author: AuthorUtils.CHARLES_BUKOWSKI
+    },
+    {
+        title: "senza averti qui",
+        poem: "Dovresti essere orgogliosa di te stessa per il modo in cui hai affrontato quest'ultimo periodo: dalle battaglie silenziose che hai combattuto, fino ai momenti in cui sei caduta ma hai comunque deciso di rialzarti per l'ennesima volta e guardare avanti. Sei una guerriera. Perciò fatti un favore e celebra la tua forza.",
+        author: AuthorUtils.SOFIA_J_ROSS
+    },
+    {
+        title: "",
+        poem: "Così tra questa immensità s'annega il pensier mio: e il naufragar m'è dolce in questo mare.",
+        author: AuthorUtils.LEOPARDI
+    },
+    {
+        title: "",
+        poem: "Le menti superiori discutono le idee; le menti normali discutono gli eventi; le menti mediocri discutono le persone",
+        author: AuthorUtils.SOCRATE
+    },
+    {
+        title: "",
+        poem: "L'uomo che fa una domanda è uno sciocco per un minuto, l'uomo che non ne fa è uno sciocco per tutta la vita.",
+        author: AuthorUtils.CONFUCIO
+    },
+    {
+        title: "",
+        poem: "L'uomo non conosce altra felicità se non quella che egli si va immaginando, e poi, finita l'illusione, ricade nel dolore di sempre.",
+        author: AuthorUtils.SOFOCLE
+    },
+    {
+        title: "",
+        poem: "un giorno mi sento come se stessi guarendo, il giorno dopo mi sto rompendo di nuovo. è come un ciclo che non ha fine.",
+        author: AuthorUtils.ANONIMO
+    },
+    {
+        title: "",
+        poem: "Accetta le cose che non puoi cambiare, e cambia le cose che non puoi accettare.",
+        author: AuthorUtils.MARCO_AURELIO
+    },
+    {
+        title: "",
+        poem: "L'amore di un solitario è il più autentico che ci possa essere. Ti ama per scelta, non per compagnia.",
+        author: AuthorUtils.CHARLES_BUKOWSKI
+    },
+    {
+        title: "",
+        poem: "Non sono sicuro di essere depresso. Voglio dire, non sono triste, ma non sono nemmeno esattamente felice. Posso ridere, scherzare e sorridere durante il giorno, ma quando sono solo di notte mi sento un po' insensibile.",
+        author: AuthorUtils.ANONIMO
+    },
+    {
+        title: "",
+        poem: "Mi sento un po' come il mare: abbastanza calma per intrapredenre nuovi rapporti umani, ma periodicamente in temepesta per allontanare tutti, per starmene sola.",
+        author: AuthorUtils.ALDA_MERINI
+    },
+    {
+        title: "",
+        poem: "E' curioso a vedere che gli uomini di molto merito hanno sempre le maniere semplici, e che sempre le maniere semplici sono prese per indizio di poco merito.",
+        author: AuthorUtils.LEOPARDI
+    },
+    {
+        title: "",
+        poem: "Ma tu chi sei che avanzando nel buio della notte inciampi nei miei più segreti pensieri?",
+        author: AuthorUtils.SHAKESPEARE
+    },
+    {
+        title: "",
+        poem: "I ricordi, come le candele, bruciano di più nel periodo natalizio.",
+        author: AuthorUtils.DICKENS
+    },
+    {
+        title: "",
+        poem: "A Natale non si fanno cattivi pensieri ma chi è solo lo vorrebbe saltare questo giorno. A tutti loro auguro di vivere un Natale in compagnia.",
+        author: AuthorUtils.ALDA_MERINI
+    },
+    {
+        title: "",
+        poem: "ricordo ancora quanto era grande il mio sorriso la sera in cui tornai a casa dopo il nostro primo bacio.",
+        author: AuthorUtils.ANONIMO
+    },
+    {
+        title: "",
+        poem: "Io penso che ci sia un eroe in tutti noi, che ci mantiene onesti, ci da forza, ci rende nobili... E alla fine ci permette di morire con dignità anche se a volte dobbiamo mostrare carattere e rinunciare alle cose che desideriamo di più, anche i nostri sogni...",
+        author: AuthorUtils.MAY_PARKER
+    },
+    {
+        title: "",
+        poem: "Non avevo da aggiungere altro verso, altra parola. Nel tuo corpo vivevo tutta la poesia.",
+        author: AuthorUtils.RITSOS
+    },
+    {
+        title: "",
+        poem: "Ma io sono predestinato ad avere l'anima perpetuamente in tempesta.",
+        author: AuthorUtils.FOSCOLO
+    },
+    {
+        title: "Succede sempre qualcosa di meraviglioso",
+        poem: "Purtroppo ormai c'è questa mentalità così fastidiosamente diffusa per cui \"semplice\" significhi di scarso valore, mentre \"complesso\" voglia dire prezioso. Non c'è niente di più falso. Le cose migliori della vita non sono complicate, anzi. Sono semplici.",
+        author: AuthorUtils.GIANLUCA_GOTTO
+    },
 ]
 
 export const getRandomizedPoems = () => {
